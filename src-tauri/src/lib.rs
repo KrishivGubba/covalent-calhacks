84142a6ef7bea971e28d6cd59ed8bab28c66e4bf
hem8705 <68325444+hem8705@users.noreply.github.com>
2026-02-03 16:31:36 -0600

    added updater and installer (aws to be impl) funcs on backend, added update button on the dashboard

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -485,6 +485,9 @@ pub fn run() {
     }
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_process::init())
         .setup(|app| {
             // Start Flask server
             let app_dir = if cfg!(dev) {
