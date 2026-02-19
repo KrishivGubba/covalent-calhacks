5e93345cef67524ad66b2d7598fadde6ce3f9c84
hem8705 <68325444+hem8705@users.noreply.github.com>
2026-02-18 21:06:08 -0600

    deleted covalent.db stuff

--- src-tauri/src/database/embeddings.rs
+++
@@ -1,9 +0,0 @@
-//Embeddings for vectorizing db
-
-use std::sync::Arc;
-use tokio::sync::RwLock;
-use serde::{Deserialize, Serialize};
-use chrono::{DateTime, Utc, Duration};
-use uuid::Uuid;
-
-use super::connection::DatabaseManager;
